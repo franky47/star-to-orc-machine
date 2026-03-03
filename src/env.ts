918f52f1d281e005de5d010ecf44ac27599de1a1
François Best <github@francoisbest.com>
2026-03-03 14:25:15 +0100

    chore: fix path

--- src/env.ts
+++ src/env.ts
@@ -3,9 +3,8 @@ import { z } from 'zod'
 const EnvSchema = z.object({
   GITHUB_WEBHOOK_SECRET: z.string().min(1, 'GITHUB_WEBHOOK_SECRET is required'),
   TARGET_URL: z
-    .string()
     .url()
-    .default('https://project-review-api.vercel.app/projects'),
+    .default('https://project-review-api.vercel.app/api/projects'),
 })
 
 export const env = EnvSchema.parse(process.env)
